d6897f05281f78224709a36187bd821f39eaefa3
RJ SHANTO <124341668+hridoyshanto@users.noreply.github.com>
2023-03-30 19:26:09 +0600

    Add files via upload

--- ROF.py
+++ ROF.py
@@ -1,3 +1,13 @@
 import os, platform, time, sys
-os.system('git pull')
-import TFOF1_enc
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.050)
+xoss('\033[0;91m Checking Your Mobile...?');time.sleep(0.50)
+time.sleep(10)
+xoss("\033[0;92m Congratulations Your Device Support This Tools")
+time.sleep(3)
+xoss('\033[0;93m JOIN MY SCRIPT GIFT GROUP')
+time.sleep(3)
+os.system('xdg-open
